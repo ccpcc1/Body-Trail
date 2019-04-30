1fe00f254cf34f9e222b0351ee7e1e6584032902
ccpc <crisrxn12@gmail.com>
2019-04-29 22:00:46 -0500

    se añadio splashcreen- con animaciones

---
+++ BodyTrail/www/js/main.js
@@ -0,0 +1,32 @@
+window.onload = inicio;
+var pantallaInicio="";
+var splash="";
+
+function inicio()
+{
+	inicializar();
+	setTimeout(plash,2000);
+	
+}
+
+function plash()
+{
+	pantallaInicio.className="container-fluid animated rotateInDownRight";
+	splash.className="ocultar";
+}
+
+function inicializar()
+{
+	pantallaInicio=document.getElementById('PantallaInicial');
+	splash=document.getElementById('PantallaSplash');
+	$('#barraCarga').jQMeter({
+    goal:'$1,000',
+    raised:'$1,000',
+    meterOrientation:'horizontal',
+    width:'320px',
+    height:'20px'
+	});
+
+}
+
+//rotateInDownRight heartBeat
